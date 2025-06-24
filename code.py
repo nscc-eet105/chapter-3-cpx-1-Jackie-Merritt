e7b7d6d34ee58cdbaaa6dfdfc9e6e4192159bc2e
Jackie Merritt <remmerritt@gmail.com>
2025-06-24 16:46:17 -0400

    code Assignment complete

---
+++ code.py
@@ -0,0 +1,30 @@
+from adafruit_circuitplayground import cp
+import time
+
+while True:
+    if cp.switch:
+        cp.red_led = True
+        cp.play_file("blip.wav")
+    else:
+        cp.red_led = False
+
+#Button
+
+    if cp.button_a:
+        cp.red_led = True
+        cp.play_file("applause_y.wav")
+    else:
+        cp.red_led = False
+
+    if cp.button_b:
+        cp.red_led = True
+        cp.play_file("bloop_x.wav")
+    else:
+        cp.red_led = False
+
+#Touch
+
+    if cp.touch_A1:
+        cp.pixels[0] = (0, 100, 100) #
+    else:
+        cp.pixels[0] = (0, 0, 0) #all off
